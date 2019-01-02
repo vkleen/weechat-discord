2e0a74e7f704969f2b017a0a29693a3f874e5ecf
Noskcaj <kcajdev@gmail.com>
2019-01-02 00:11:37 -0700

    Wrap message handling code in main wrapper

--- src/discord/event_handler.rs
+++ src/discord/event_handler.rs
@@ -16,38 +16,40 @@ impl EventHandler for Handler {
     // Called when a message is received
     fn message(&self, _: Context, msg: Message) {
         let string_channel = utils::buffer_id_from_channel(&msg.channel_id);
-        if let Some(buffer) = Buffer::search(&string_channel) {
-            let muted = utils::buffer_is_muted(&buffer);
-            let notify = !msg.is_own() && !muted;
-            on_main! {{printing::print_msg(&buffer, &msg, notify)}};
-        } else {
-            match msg.channel_id.to_channel() {
-                chan @ Ok(Channel::Private(_)) => {
-                    if let Some(buffer) = Buffer::search(&string_channel) {
-                        let muted = utils::buffer_is_muted(&buffer);
-                        let notify = !msg.is_own() && !muted;
-                        on_main! {{ printing::print_msg(&buffer, &msg, notify) }};
-                    } else {
-                        // TODO: Implement "switch_to"
-                        buffers::create_buffer_from_dm(
-                            chan.unwrap(),
-                            &CACHE.read().user.name,
-                            false,
-                        );
+        on_main! {{
+            if let Some(buffer) = Buffer::search(&string_channel) {
+                let muted = utils::buffer_is_muted(&buffer);
+                let notify = !msg.is_own() && !muted;
+                printing::print_msg(&buffer, &msg, notify);
+            } else {
+                match msg.channel_id.to_channel() {
+                    chan @ Ok(Channel::Private(_)) => {
+                        if let Some(buffer) = Buffer::search(&string_channel) {
+                            let muted = utils::buffer_is_muted(&buffer);
+                            let notify = !msg.is_own() && !muted;
+                            printing::print_msg(&buffer, &msg, notify);
+                        } else {
+                            // TODO: Implement "switch_to"
+                            buffers::create_buffer_from_dm(
+                                chan.unwrap(),
+                                &CACHE.read().user.name,
+                                false,
+                            );
+                        }
                     }
-                }
-                chan @ Ok(Channel::Group(_)) => {
-                    if let Some(buffer) = Buffer::search(&string_channel) {
-                        let muted = utils::buffer_is_muted(&buffer);
-                        let notify = !msg.is_own() && !muted;
-                        on_main! {{ printing::print_msg(&buffer, &msg, notify) }};
-                    } else {
-                        buffers::create_buffer_from_group(chan.unwrap(), &CACHE.read().user.name);
+                    chan @ Ok(Channel::Group(_)) => {
+                        if let Some(buffer) = Buffer::search(&string_channel) {
+                            let muted = utils::buffer_is_muted(&buffer);
+                            let notify = !msg.is_own() && !muted;
+                            printing::print_msg(&buffer, &msg, notify);
+                        } else {
+                            buffers::create_buffer_from_group(chan.unwrap(), &CACHE.read().user.name);
+                        }
                     }
+                    _ => {}
                 }
-                _ => {}
             }
-        }
+        }};
     }
 
     // fn message_delete(&self, _: Context, channel: ChannelId, message: MessageId) {}

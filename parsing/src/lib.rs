e9acd4ae458dcc1c72915347d2a74431e2c4e46f
Noskcaj <kcajdev@gmail.com>
2019-05-20 16:45:22 -0400

    Switch to SimpleAST

--- parsing/src/lib.rs
+++ parsing/src/lib.rs
@@ -1,146 +1,24 @@
-use pest::Parser;
+pub use simple_ast::MarkdownNode;
+use simple_ast::{Parser, Rule, Styled};
 
-#[derive(pest_derive::Parser)]
-#[grammar = "discord_grammar.pest"]
-pub struct MarkdownParser;
+pub fn parse_markdown(str: &str) -> Styled<MarkdownNode> {
+    use simple_ast::markdown_rules::*;
+    let rules: &[&Rule<MarkdownNode>] = &[
+        &Escape,
+        &Newline,
+        &Bold,
+        &Underline,
+        &Italic,
+        &Strikethrough,
+        &Spoiler,
+        &Code,
+        &InlineCode,
+        &Text,
+    ];
 
-#[derive(Debug, PartialEq, Clone)]
-pub struct Styled(pub Vec<Style>);
-
-impl Styled {
-    pub fn as_markdown(&self) -> String {
-        self.0
-            .iter()
-            .map(Style::as_markdown)
-            .collect::<Vec<_>>()
-            .join("")
-    }
-}
-
-#[derive(Debug, PartialEq, Clone)]
-pub enum Style {
-    Bold(Vec<Style>),
-    Italic(Vec<Style>),
-    Underline(Vec<Style>),
-    Strikethrough(Vec<Style>),
-    Plain(String),
-}
-
-impl Style {
-    // TODO: take into account _/* italic syntax
-    pub fn as_markdown(&self) -> String {
-        use Style::*;
-        match self {
-            Bold(style) => format!(
-                "**{}**",
-                style
-                    .iter()
-                    .map(Style::as_markdown)
-                    .collect::<Vec<_>>()
-                    .join("")
-            ),
-            Italic(style) => format!(
-                "_{}_",
-                style
-                    .iter()
-                    .map(Style::as_markdown)
-                    .collect::<Vec<_>>()
-                    .join("")
-            ),
-            Underline(style) => format!(
-                "__{}__",
-                style
-                    .iter()
-                    .map(Style::as_markdown)
-                    .collect::<Vec<_>>()
-                    .join("")
-            ),
-            Strikethrough(style) => format!(
-                "~~{}~~",
-                style
-                    .iter()
-                    .map(Style::as_markdown)
-                    .collect::<Vec<_>>()
-                    .join("")
-            ),
-            Plain(string) => string.to_owned(),
-        }
-    }
-}
-
-fn lower(pair: pest::iterators::Pair<Rule>) -> Vec<Style> {
-    let rule = pair.as_rule();
-    match rule {
-        Rule::all => {
-            let mut result = Vec::new();
-            for pair in pair.into_inner() {
-                result.extend(lower(pair))
-            }
-            result
-        }
-        Rule::italic => {
-            let mut result = Vec::new();
-            for pair in pair.into_inner() {
-                result.extend(lower(pair))
-            }
-            vec![Style::Italic(result)]
-        }
-        Rule::underline_inner | Rule::strike_inner | Rule::bold_inner | Rule::italic_inner => {
-            let content = pair.as_str();
-            match pair.into_inner().next() {
-                Some(inner) => lower(inner),
-                None => vec![Style::Plain(content.to_owned())],
-            }
-        }
-        Rule::bold => {
-            let mut result = Vec::new();
-            for pair in pair.into_inner() {
-                result.extend(lower(pair))
-            }
-            vec![Style::Bold(result)]
-        }
-        Rule::underline => {
-            let mut result = Vec::new();
-            for pair in pair.into_inner() {
-                result.extend(lower(pair))
-            }
-            vec![Style::Underline(result)]
-        }
-        Rule::strike => {
-            let mut result = Vec::new();
-            for pair in pair.into_inner() {
-                result.extend(lower(pair))
-            }
-            vec![Style::Strikethrough(result)]
-        }
-        Rule::plain | Rule::WS => vec![Style::Plain(pair.as_str().to_owned())],
-
-        _ => unimplemented!(),
-    }
-}
-
-pub fn parse_markdown(str: &str) -> Styled {
-    // Should be infallible
-    let mut rules = MarkdownParser::parse(Rule::all, str).unwrap();
-
-    Styled(lower(rules.next().unwrap()))
+    Parser::with_rules(rules).parse(str)
 }
 
 pub fn weechat_arg_strip(str: &str) -> String {
     str.trim().replace(' ', "_")
-}
-
-#[cfg(test)]
-mod test {
-    #[test]
-    fn test() {
-        use super::{Style::*, Styled};
-        let target = Styled(vec![Bold(vec![
-            Italic(vec![Plain("Hi".to_string())]),
-            Plain(" ".to_string()),
-            Underline(vec![Plain("there".to_string())]),
-        ])]);
-
-        assert_eq!(target, super::parse_markdown("**_Hi_ __there__**"));
-    }
 }
--- src/discord/format.rs
+++ src/discord/format.rs
@@ -1,71 +1,64 @@
 use crate::ffi::color_codes;
-use parsing::{self, Style};
+use parsing::{self, MarkdownNode};
+use std::rc::Rc;
+use std::sync::RwLock;
 
 pub fn discord_to_weechat(msg: &str) -> String {
-    let ast = parsing::parse_msg(msg).unwrap_or_else(Vec::new);
-    let mut result = String::new();
-    for node in ast {
-        match node {
-            Style::Text(txt) => result.push_str(&txt),
-            Style::Code(code) => {
-                // TODO: Code blocks
-                result.push_str(&color_codes("*8"));
-                result.push_str(&code);
-                result.push_str(&color_codes("reset"));
-            }
-            Style::Bold(bold) => {
-                result.push_str(&color_codes("bold"));
-                result.push_str(&bold);
-                result.push_str(&color_codes("-bold"));
-            }
-            Style::Italic(italic) => {
-                result.push_str(&color_codes("italic"));
-                result.push_str(&italic);
-                result.push_str(&color_codes("-italic"));
-            }
-            Style::BoldItalics(bold_italic) => {
-                result.push_str(&color_codes("bold"));
-                result.push_str(&color_codes("italic"));
-                result.push_str(&bold_italic);
-                result.push_str(&color_codes("-bold"));
-                result.push_str(&color_codes("-italic"));
-            }
-            Style::Underline(under) => {
-                result.push_str(&color_codes("underline"));
-                result.push_str(&under);
-                result.push_str(&color_codes("-underline"));
-            }
-            Style::UnderlineBold(under_bold) => {
-                result.push_str(&color_codes("bold"));
-                result.push_str(&color_codes("underline"));
-                result.push_str(&under_bold);
-                result.push_str(&color_codes("-bold"));
-                result.push_str(&color_codes("-underline"));
-            }
-            Style::UnderlineItalics(under_italics) => {
-                result.push_str(&color_codes("italic"));
-                result.push_str(&color_codes("underline"));
-                result.push_str(&under_italics);
-                result.push_str(&color_codes("-italic"));
-                result.push_str(&color_codes("-underline"));
-            }
-            Style::UnderlineBoldItalics(under_bold_italics) => {
-                result.push_str(&color_codes("italic"));
-                result.push_str(&color_codes("bold"));
-                result.push_str(&color_codes("underline"));
-                result.push_str(&under_bold_italics);
-                result.push_str(&color_codes("-italic"));
-                result.push_str(&color_codes("-bold"));
-                result.push_str(&color_codes("-underline"));
-            }
-            Style::Strikethrough(strikethrough) => {
-                result.push_str(&color_codes("red"));
-                result.push_str("~~");
-                result.push_str(&strikethrough);
-                result.push_str("~~");
-                result.push_str(&color_codes("resetcolor"));
-            }
+    let ast = parsing::parse_markdown(msg);
+
+    let mut out = String::new();
+    for node in &ast.0 {
+        eprintln!("{:#?}", node.read().unwrap());
+        out.push_str(&discord_to_weechat_reducer(&*node.read().unwrap()))
+    }
+    out
+}
+
+fn collect_styles(styles: &[Rc<RwLock<MarkdownNode>>]) -> String {
+    styles
+        .iter()
+        .map(|s| discord_to_weechat_reducer(&*s.read().unwrap()))
+        .collect::<Vec<_>>()
+        .join("")
+}
+
+// TODO: Spoilers, code syntax highlighting?
+fn discord_to_weechat_reducer(node: &MarkdownNode) -> String {
+    use MarkdownNode::*;
+    match node {
+        Bold(styles) => format!(
+            "{}{}{}",
+            color_codes("bold"),
+            collect_styles(styles),
+            color_codes("-bold")
+        ),
+        Italic(styles) => format!(
+            "{}{}{}",
+            color_codes("italic"),
+            collect_styles(styles),
+            color_codes("-italic")
+        ),
+        Underline(styles) => format!(
+            "{}{}{}",
+            color_codes("underline"),
+            collect_styles(styles),
+            color_codes("-underline")
+        ),
+        Strikethrough(styles) => format!(
+            "{}~~{}~~{}",
+            color_codes("red"),
+            collect_styles(styles),
+            color_codes("-red")
+        ),
+        Spoiler(styles) => format!(
+            "{}||{}||{}",
+            color_codes("italic"),
+            collect_styles(styles),
+            color_codes("-italic")
+        ),
+        Text(string) => string.to_owned(),
+        InlineCode(string) | Code(_, string) => {
+            format!("{}{}{}", color_codes("*8"), string, color_codes("reset"))
         }
     }
-    result
 }
